32d48669aeea02a4714cab2107e5d3e94d0f7437
Bhavya Talwar <140486068+bhavyatalwar04@users.noreply.github.com>
2025-05-30 17:11:19 +0530

    30may

--- CLASS/first.java
+++ CLASS/first.java
@@ -1,24 +1,16 @@
-class first
-{
-    public static void main(String args[])
-    {
-    System.out.println("Hello World");
+
+import java.util.*;
+
+class main {
+
+    public static void main(String args[]) {
+        int n1, n2;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of students: ");
+        n1 = sc.nextInt();
+        System.out.println("Enter number of subjects: ");
+        n2 = sc.nextInt();
+        int[]
+  
     }
 }
-//Byte code makes java platform independent
-// File name and class name should be same
-//Public is access modifier
-// Classes here are String and System as first letter is capital
-// Main is called by JVM
-// Main method in C and CPP is called by OS
-// Static keyword is used to call main method without creating object
-// Void is return type
-// Public keyword is an access modifier for the method
-// String args[] is command line argument means it creates a array of string arguments that  can take taking one argument of type string array
-// To call a method we write .out (object reference)
-// Println is a method
-// Set temporary path using set path
-// Permanent path using set path
-// Java is high level language 
-// Compiler is a transalator convert high level language into machine code or bvinary code. Not hardware dependent.
-// Assembler is a transalator convert low level language into machine code or bvinary code. It is hardware dependent.P
